043bc6e3aaa61408c5b59e8e9e1ebb485bce75e3
James Lomax <james.lmx@gmail.com>
2020-12-15 13:02:44 +0000

    (pointless overengineered) optimisations!

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <unordered_map>
+#include <array>
 #include <cassert>
+#include <chrono>
 
 using namespace std;
 
+template <class MapType = map<uint32_t, uint32_t>>
 class Game
 {
 public:
-    Game(vector<size_t>&& nums);
+    Game(vector<uint32_t> nums);
 
-    size_t step();
+    uint32_t step();
 
-    size_t step_til(size_t count);
+    uint32_t step_til(uint32_t count);
 
-    size_t last_num() const
+    uint32_t last_num() const
     {
         return last;
     }
 
 private:
-    map<size_t, size_t> seen;
-    size_t count;
-    size_t last;
+    MapType seen;
+    uint32_t count;
+    uint32_t last;
 
 };
 
-Game::Game(vector<size_t>&& nums)
+template <class MapType>
+Game<MapType>::Game(vector<uint32_t> nums)
 {
-    for (size_t i = 0; i < nums.size() - 1; i++)
+    for (uint32_t i = 0; i < nums.size() - 1; i++)
     {
         seen[nums[i]] = i + 1;
     }
@@ -36,12 +41,13 @@ Game::Game(vector<size_t>&& nums)
     last = nums.back();
 }
 
-size_t Game::step()
+template <class MapType>
+uint32_t Game<MapType>::step()
 {
     if (seen.count(last) > 0)
     {
-        size_t last_idx = seen[last];
-        size_t elapsed = count - last_idx;
+        uint32_t last_idx = seen[last];
+        uint32_t elapsed = count - last_idx;
         
         seen[last] = count;
 
@@ -58,7 +64,8 @@ size_t Game::step()
     return last;
 }
 
-size_t Game::step_til(size_t target)
+template <class MapType>
+uint32_t Game<MapType>::step_til(uint32_t target)
 {
     while (count < target)
     {
@@ -85,14 +92,57 @@ void test()
     }
 }
 
+template<class MapType>
+void p2_time(string map_t_name, vector<uint32_t>& nums)
+{
+    Game<MapType> game(nums);
+    auto start = chrono::high_resolution_clock::now();
+    uint32_t p2 = game.step_til(30000000);
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    cout << "Part 2 = " << p2 << " (with " << map_t_name << " in " << duration.count() << "ms)" << endl;
+}
+
+class HugeArrayMap
+{
+    const static uint32_t UNSEEN = (uint32_t)-1;
+
+public:
+    HugeArrayMap()
+        : a(new array<uint32_t, 32000000>)
+    {
+        a->fill(UNSEEN);
+    }
+    
+    ~HugeArrayMap()
+    {
+        delete a;
+    }
+
+    uint32_t& operator[](uint32_t n)
+    {
+        return (*a)[n];
+    }
+
+    uint32_t count(uint32_t n)
+    {
+        return (*a)[n] == UNSEEN ? 0 : 1;
+    }
+
+private:
+    array<uint32_t, 32000000>* a;
+};
+
 void main()
 {
     test();
 
-    Game game({ 12,1,16,3,11,0 });
-    size_t p1 = game.step_til(2020);
+    std::vector<uint32_t> nums = { 12,1,16,3,11,0 };
+    Game game(nums);
+    uint32_t p1 = game.step_til(2020);
     cout << "Part 1 = " << p1 << endl;
 
-    size_t p2 = game.step_til(30000000);
-    cout << "Part 2 = " << p2 << endl;
+    p2_time<map<uint32_t, uint32_t>>("std::map", nums);
+    p2_time<unordered_map<uint32_t, uint32_t>>("std::unordered_map", nums);
+    p2_time<HugeArrayMap>("HugeArrayMap", nums);
 }
